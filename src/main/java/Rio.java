9ea75d141262457cb181a3cdf4fa0db7c610dcdf
Carameow <iywudla@gmail.com>
2025-08-22 11:00:07 +0800

    Rename Duke to Rio and add greeting and exit
    Replaces the Duke class and related references with Rio, including updates to README files, test scripts, and expected output. The new Rio class introduces a revised logo and greeting message, reflecting the project's new identity.

---
+++ src/main/java/Rio.java
@@ -0,0 +1,27 @@
+public class Rio {
+
+    public void presentation() {
+        String logo = " ____  _       \n"
+                + "|  _ \\(_) ___  \n"
+                + "| |_) | |/ _ \\ \n"
+                + "|  _ <| | |_| |\n"
+                + "|_| \\_\\_|\\___/ \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+
+    public static void main(String[] args) {
+        String endLine = "____________________________________________________________";
+
+        System.out.println(endLine);
+
+        System.out.println("Hello, I'm Rio - your personal assistant.");
+        System.out.println("What can I do for you today?");
+
+        System.out.println(endLine);
+
+        System.out.println("Bye. Hope to see you again sometime soon!");
+
+        System.out.println(endLine);
+    }
+}
