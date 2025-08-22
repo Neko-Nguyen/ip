d295c0b6752d9799cbf11ddcb740a05a0aa57945
Neko-Nguyen <iywudla@gmail.com>
2025-08-22 16:34:24 +0800

    level-1

--- src/main/java/Rio.java
+++ src/main/java/Rio.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Rio {
+    static String endLine = "   ____________________________________________________________";
 
     public void presentation() {
         String logo = " ____  _       \n"
@@ -10,18 +13,28 @@ public class Rio {
         System.out.println("Hello from\n" + logo);
     }
 
+    public boolean doTask(String inp) {
+        if (inp.equals("bye")) {
+            System.out.println("    Bye. Hope to see you again sometime soon!");
+            return false;
+        }
+        System.out.println("    " + inp);
+        return true;
+    }
+
     public static void main(String[] args) {
-        String endLine = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        Rio rio = new Rio();
 
         System.out.println(endLine);
 
-        System.out.println("Hello, I'm Rio - your personal assistant.");
-        System.out.println("What can I do for you today?");
+        System.out.println("    Hello, I'm Rio - your personal assistant.");
+        System.out.println("    What can I do for you today?");
 
         System.out.println(endLine);
 
-        System.out.println("Bye. Hope to see you again sometime soon!");
-
-        System.out.println(endLine);
+        while (rio.doTask(scanner.nextLine())) {
+            System.out.println(endLine);
+        }
     }
 }
