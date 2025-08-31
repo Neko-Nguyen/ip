3d35be5d2a89c2c332d6d1e642d4f9514df9f175
Neko-Nguyen <iywudla@gmail.com>
2025-08-31 14:29:09 +0800

    Add JUnit tests
    Introduced unit tests for Deadline and ToDo classes to verify their toString() methods.

---
+++ src/test/java/rio/task/ToDoTest.java
@@ -0,0 +1,11 @@
+package rio.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void toStringTest() {
+        assertEquals("[T][ ] return book", new ToDo("return book").toString());
+    }
+}
